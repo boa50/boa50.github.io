40502309bea55a24453b0dcecb591d0cd7a03663
boa50 <bruno.albuquerque50@gmail.com>
2023-04-24 05:04:39 +0930

    Quick fix to show the carousel images for any project

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,18 +1,19 @@
 var slideIndex = 1;
-showDivs(slideIndex);
+showDivs(slideIndex, "proj01");
+showDivs(slideIndex, "proj02");
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusDivs(n, proj = "proj01") {
+  showDivs(slideIndex += n, proj);
 }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
+function currentDiv(n, proj = "proj01") {
+  showDivs(slideIndex = n, proj);
 }
 
-function showDivs(n) {
+function showDivs(n, proj = "proj01") {
   var i;
-  var x = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("demo");
+  var x = document.getElementsByClassName("slides " + proj);
+  var dots = document.getElementsByClassName("demo " + proj);
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
