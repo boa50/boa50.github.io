46fec385e48d4723c018111d9d6700a7046325da
boa50 <bruno.albuquerque50@gmail.com>
2023-04-25 04:58:08 +0930

    Fixed the problem of selecting images of diferent carousels in the page

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,27 +1,33 @@
-var slideIndex = 1;
-showDivs(slideIndex, "proj01");
-showDivs(slideIndex, "proj02");
+var slideIndexes = {
+  proj01: 1,
+  proj02: 1,
+  proj03: 1
+};
 
-function plusDivs(n, proj = "proj01") {
-  showDivs(slideIndex += n, proj);
+showDivs(slideIndexes["proj01"], "proj01");
+showDivs(slideIndexes["proj02"], "proj02");
+showDivs(slideIndexes["proj03"], "proj03");
+
+function plusDivs(n, proj) {
+  showDivs(slideIndexes[proj] += n, proj);
 }
 
-function currentDiv(n, proj = "proj01") {
-  showDivs(slideIndex = n, proj);
+function currentDiv(n, proj) {
+  showDivs(slideIndexes[proj] = n, proj);
 }
 
-function showDivs(n, proj = "proj01") {
+function showDivs(n, proj) {
   var i;
   var x = document.getElementsByClassName("slides " + proj);
   var dots = document.getElementsByClassName("demo " + proj);
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  if (n > x.length) {slideIndexes[proj] = 1}
+  if (n < 1) {slideIndexes[proj] = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" w3-white", "");
   }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
+  x[slideIndexes[proj] - 1].style.display = "block";  
+  dots[slideIndexes[proj] - 1].className += " w3-white";
 }
